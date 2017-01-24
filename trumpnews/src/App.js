673d0635c3e0547b1fc5db02f815499608c139f7
Jan Eirik <janeirik@hallstensen.net>
2017-01-24 20:29:21 +0100

    Styling

--- trumpnews/src/App.js
+++ trumpnews/src/App.js
@@ -6,13 +6,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <nav className="navbar navbar-nc navbar-top" role="navigation">
           <div className="container">
             <div className="navbar-header">
-
+              <h2 className="logo">Trumpnews</h2>
             </div>
           </div>
         </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+                {/* Kategorier */}
+            </div>
+            <div className="col-md-8">
+                <h1>Velkommen til fagkveld!</h1>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
