450a4d8511c2b1fa61807e657cd5b3b91709593f
Jan Eirik <janeirik@hallstensen.net>
2017-01-24 20:07:59 +0100

    Frontend initial commit

---
+++ trumpnews/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
